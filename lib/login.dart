209ab23664b334bf83d220e741289a5c7cd09151
RICU <rich.chuang@tw.pwc.com>
2019-01-08 22:03:58 +0800

    MDC-101: Add TextField widgets.

--- lib/login.dart
+++ lib/login.dart
@@ -40,7 +40,23 @@ class _LoginPageState extends State<LoginPage> {
             // TODO: Wrap Username with AccentColorOverride (103)
             // TODO: Remove filled: true values (103)
             // TODO: Wrap Password with AccentColorOverride (103)
-            // TODO: Add TextField widgets (101)
+            // [Name]
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Username',
+              ),
+            ),
+            // spacer
+            SizedBox(height: 12.0),
+            // [Password]
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
             // TODO: Add button bar (101)
           ],
         ),
