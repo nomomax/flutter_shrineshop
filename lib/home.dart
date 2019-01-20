b73a9bbbcf11d0f9973701a16aef2c1d4b522c72
RICU <rich.chuang@tw.pwc.com>
2019-01-20 21:29:17 +0800

    MDC-103: Change the layout.

--- lib/home.dart
+++ lib/home.dart
@@ -13,64 +13,12 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 import 'model/products_repository.dart';
 import 'model/product.dart';
+import 'Supplemental/asymmetric_view.dart';
 
 class HomePage extends StatelessWidget {
-  List<Card> _buildGridCards(BuildContext context) {
-    List<Product> products = ProductsRepository.loadProducts(Category.all);
-
-    if (products == null || products.isEmpty) {
-      return const <Card>[];
-    }
-
-    final ThemeData theme = Theme.of(context);
-    final NumberFormat formatter = NumberFormat.simpleCurrency(
-      locale: Localizations.localeOf(context).toString());
-
-    return products.map((product) {
-      return Card(
-        elevation: 0.0,
-        clipBehavior: Clip.antiAlias,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            AspectRatio(
-              aspectRatio: 18.0 / 11.0,
-              child: Image.asset(
-                product.assetName,
-                package: product.assetPackage,
-                fit: BoxFit.fitWidth,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    product == null ? '' : product.name,
-                    style: theme.textTheme.button,
-                    softWrap: false,
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 1,
-                  ),
-                  SizedBox(height: 4.0,),
-                  Text(
-                    product == null ? '' : formatter.format(product.price),
-                    style: theme.textTheme.caption,
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      );
-    }).toList();
-  }
 
   // TODO: Add a variable for Category (104)
   @override
@@ -110,12 +58,7 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        padding: EdgeInsets.all(16.0),
-        childAspectRatio: 8.0 / 9.0,
-        children: _buildGridCards(context),
-      ),
+      body: AsymmetricView(products: ProductsRepository.loadProducts(Category.all)),
     );
   }
 }
